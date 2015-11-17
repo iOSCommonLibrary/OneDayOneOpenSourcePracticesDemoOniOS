07db01292d5b898e79014ff5ce10b8304b347b47
suxinde2009 <suxinde2009@126.com>
2015-11-18 00:26:53 +0800

    ProtocolKitDemo Init

---
+++ ObjC/ProtocolKitDemo/ProtocolKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtocolKitDemo
+//
+//  Created by su xinde on 15/11/18.
+//  Copyright © 2015年 com.su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
