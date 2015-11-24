4a5653285909dc5f21b20e6f50a9c3e0ead08925
suxinde2009 <suxinde2009@126.com>
2015-11-25 00:28:10 +0800

    Learning materials from PaicHyperionDev(https://github.com/PaicHyperionDev)

---
+++ ObjC/NSFetchedResultControllerDemo/NSFetchedResultControllerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSFetchedResultControllerDemo
+//
+//  Created by zj－db0367 on 15/11/24.
+//  Copyright © 2015年 com.su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
